6f44f493e87d44d7a786e59c379f5448933c6c6b
myUsername <myemail@gmail.com>
2023-02-18 18:38:08 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -11,8 +12,8 @@ int main(void)
 {
 	int n;
 
-	"srand(time(0));
-	n = rand() - 'RAND_MAX' / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
