ce1de0a377e1c8b01963888f716a93376fcb48c6
memorymukonda <memmoemukoe@gmaol.com>
2023-04-20 13:48:54 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/**
- * main -  entry point
- * return: always 0 (success)
- */
 
+/**
+ * Main -Entry point
+ *
+ * Return: Always 0 (succes)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
