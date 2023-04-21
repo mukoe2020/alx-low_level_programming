b276fbbd889941f9a510a858ea2be6e6a7d04843
memorymukonda <memmoemukoe@gmaol.com>
2023-04-21 13:32:05 -0700

    lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Description: alphabet in lowercase
+ *Return: always 0
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
