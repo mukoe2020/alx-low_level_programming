2e688921f1bc2ee58adb0143e5f4700ff783109b
memorymukonda <memmoemukoe@gmaol.com>
2023-04-28 11:57:28 -0700

    he numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Entry point
+  * Description - 'prints numbers 0 to 9'
+  * Return: Always 0 (sucess)
+  */
+
+void print_numbers(void)
+{
+	int h;
+
+	for (h = 48; h <= 57; h++)
+	{
+	_putchar(h);
+	}
+	_putchar('\n');
+}
