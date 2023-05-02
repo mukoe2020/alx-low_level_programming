a75c1278a3b076f6bb5e9261a58d8e672d938749
memorymukonda <memmoemukoe@gmaol.com>
2023-05-02 12:33:55 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+ #include "main.h"
+
+/**
+  * rev_string - Entry point
+  * Description - 'printing a string in reverse, but in halves'
+  * @s: string to be reversed
+  * Return: void
+  */
+ void rev_string(char *s)
+{
+           char x;
+           int length;
+           int half;
+           int i;
+           i = 0;
+ 
+          for (length = 0; s[length] != '\0'; length++)
+          {
+                  half = length / 2;
+          }
+          while (half--)
+          {
+                  x = s[length - i - 1];
+                  s[length - i - 1] = s[i];
+                  s[i] = x;
+                  i++;
+          }
+}
+                          
