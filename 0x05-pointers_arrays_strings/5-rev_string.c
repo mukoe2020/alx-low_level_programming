d04d6e28ae8f147f2f3c170ae39227f2d25290a7
memorymukonda <memmoemukoe@gmaol.com>
2023-05-02 12:44:56 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,23 @@
   */
  void rev_string(char *s)
 {
-           char x;
-           int length;
-           int half;
-           int i;
-           i = 0;
+	char x;
+	int length;
+	int half;
+	int i;
+	i = 0;
  
-          for (length = 0; s[length] != '\0'; length++)
-          {
-                  half = length / 2;
-          }
-          while (half--)
-          {
-                  x = s[length - i - 1];
-                  s[length - i - 1] = s[i];
-                  s[i] = x;
-                  i++;
-          }
+for (length = 0; s[length] != '\0'; length++)
+{
+	half = length / 2;
+}
+while (half--)
+{
+	x = s[length - i - 1];
+	[length - i - 1] = s[i];
+	s[i] = x;
+	i++;
+}
+
 }
                           
