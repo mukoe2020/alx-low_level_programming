7f203c5ed6e00fb3c5bb1d122e8efbea94ce8312
memorymukonda <memmoemukoe@gmaol.com>
2023-05-02 12:00:46 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Entry point
+  * Description - 'function taking a pointer to an int as parameter'
+  * @n: An integer input pointer
+  * Return: 0 (success)
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
