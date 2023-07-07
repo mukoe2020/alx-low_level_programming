28d36424f9d077b7d0b523d17f038fd9325b90ba
memoe <memoemukoe@gmail.com>
2023-07-06 18:30:25 -0700

    function that checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - Entry point
+ * Description - 'checks if a machine is a little or a big endian'
+ * Return: 0 if it is big, 1 if it is little
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &h;
+
+	return (*c);
+}i
