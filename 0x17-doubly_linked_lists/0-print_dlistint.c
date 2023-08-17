84502085536170d9d173ef3da65585efac832b20
memoe <memoemukoe@gmail.com>
2023-08-17 03:19:06 -0700

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a doubly linked list.
- * @h: Pointer to the head of the doubly linked list.
- * Return: The number of nodes in the list.
+ * print_dlistint - prints all elements in a linked list
+ * @h: head of linked list
+ * Return: number of nodes in the linked list
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t countnodes = 0;
 
 	while (h != NULL)
 	{
+		countnodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-
-		count++;
 	}
-
-	return (count);
+	return (countnodes);
 }
